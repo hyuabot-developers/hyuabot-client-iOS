7f10f4d81cf5893e5848b0d3639aa1840ac43386
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-03-26 17:24:50 +0900

    Add tabview for each function

--- hyuabot/ContentView.swift
+++ hyuabot/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView{
+            Text("Shuttle").tabItem{
+                Image(systemName: "bus")
+                Text("Shuttle")
+            }
+            
+            Text("Subway").tabItem{
+                Image(systemName: "tram")
+                Text("Subway")
+            }
+            
+            Text("Bus").tabItem{
+                Image(systemName: "bus.doubledecker")
+                Text("Bus")
+            }
+            
+            Text("Cafeteria").tabItem{
+                Image(systemName: "fork.knife")
+                Text("Cafeteria")
+            }
+            
+            Text("Reading room").tabItem{
+                Image(systemName: "book")
+                Text("Reading room")
+            }
+            
+            Text("Map").tabItem{
+                Image(systemName: "map")
+                Text("Map")
+            }
+            
+            Text("Contacts").tabItem{
+                Image(systemName: "phone")
+                Text("Contacts")
+            }
+            
+            Text("Calendar").tabItem{
+                Image(systemName: "calendar")
+                Text("Calendar")
+            }
+            
+            Text("Setting").tabItem{
+                Image(systemName: "gear")
+                Text("Setting")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+          ContentView()
+            .environment(\.locale, .init(identifier: "en"))
+          ContentView()
+            .environment(\.locale, .init(identifier: "ko"))
+        }
     }
 }

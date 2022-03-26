fb4a17eeb27a2a836fea47ce3665b175e0084a29
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-03-26 16:46:33 +0900

    initial commit

---
+++ hyuabot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hyuabot
+//
+//  Created by 이정인 on 2022/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hyuabot/hyuabotApp.swift
@@ -0,0 +1,17 @@
+//
+//  hyuabotApp.swift
+//  hyuabot
+//
+//  Created by 이정인 on 2022/03/26.
+//
+
+import SwiftUI
+
+@main
+struct hyuabotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
